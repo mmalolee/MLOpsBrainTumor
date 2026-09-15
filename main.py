1da36eda3f73b6ea2dc15f96909f7db2b2f09307
Mikołaj <malolepszy.mikolaj00@gmail.com>
2026-09-15 21:31:30 +0200

    paths cfg

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from braintumorops!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/brain_tumor_ops/configs/paths.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass
+class PathsConfig:
+    ROOT_DIR = Path(__file__).resolve().parents[3]
+
+    @property
+    def source_raw_data_dir(self):
+        return self.ROOT_DIR / "data" / "raw"
+
+    @property
+    def source_raw_training_data_dir(self):
+        return self.source_raw_data_dir / "Training"
+
+    @property
+    def source_raw_testing_data_dir(self):
+        return self.source_raw_data_dir / "Testing"
+
+
+if __name__ == "__main__":
+    p = PathsConfig()
+
+    print(p.ROOT_DIR)
+    print(p.source_raw_data_dir)
+    print(p.source_raw_training_data_dir)
+    print(p.source_raw_testing_data_dir)

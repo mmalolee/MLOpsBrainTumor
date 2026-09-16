14be19b3a5d887b7733fe3ec1a22fb087c4f6b8b
Mikołaj <malolepszy.mikolaj00@gmail.com>
2026-09-16 19:43:13 +0200

    preprocessor

---
+++ src/brain_tumor_ops/data_preparation/preprocessing.py
@@ -0,0 +1,43 @@
+from torchvision import transforms
+
+from src.brain_tumor_ops.configs.preprocessing import DataPreprocessingConfig
+
+
+class DataPreprocessor:
+    def __init__(
+        self,
+        data_preprocessing_config: DataPreprocessingConfig,
+    ):
+        self.data_preprocessing_config = data_preprocessing_config
+
+    @staticmethod
+    def _grayscale() -> transforms.Grayscale:
+        return transforms.Grayscale()
+
+    def _resize(self) -> transforms.Resize:
+        return transforms.Resize(
+            size=(
+                self.data_preprocessing_config.img_size,
+                self.data_preprocessing_config.img_size,
+            )
+        )
+
+    @staticmethod
+    def _to_tensor() -> transforms.ToTensor:
+        return transforms.ToTensor()
+
+    def _normalize(self) -> transforms.Normalize:
+        return transforms.Normalize(
+            mean=self.data_preprocessing_config.mean,
+            std=self.data_preprocessing_config.std,
+        )
+
+    def build_transforms(self) -> transforms.Compose:
+        return transforms.Compose(
+            [
+                self._grayscale(),
+                self._resize(),
+                self._to_tensor(),
+                self._normalize(),
+            ]
+        )

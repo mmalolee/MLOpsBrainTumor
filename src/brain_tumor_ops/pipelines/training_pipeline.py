9e85a3fea85942a3c73e02f8cb15a131ba45ec4e
Mikołaj <malolepszy.mikolaj00@gmail.com>
2026-09-17 22:39:42 +0200

    init

--- src/brain_tumor_ops/pipelines/training_pipeline.py
+++ src/brain_tumor_ops/pipelines/training_pipeline.py
@@ -9,9 +9,13 @@ class TrainingPipeline:
     def __init__(
         self,
         paths_config: PathsConfig,
-        data_preprocessing_config: DataPreprocessingConfig,
         data_loader_config: DataLoaderConfig,
         model_config: ModelConfig,
         training_config: TrainingConfig,
+        data_preprocessing_config: DataPreprocessingConfig,
     ):
-        pass
+        self.paths_config = paths_config
+        self.data_loader_config = data_loader_config
+        self.model_config = model_config
+        self.training_config = training_config
+        self.data_preprocessing_config = data_preprocessing_config
